271530f19872284f7dcf5d2d47f91b086456d7dc
DAniel Cantarella Alves <danielcantarellalaves@gmail.com>
2024-06-27 08:11:24 -0300

    problelmas

--- app/src/main/java/com/example/apptcc/falecono.java
+++ app/src/main/java/com/example/apptcc/falecono.java
@@ -7,12 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class falecono extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_falecono);
 
         Button confirmfaleconosco = findViewById(R.id.btn_confirma);
 
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
 
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,
+                Intent intent = new Intent(falecono.this,
                         confirmfaleconosco.class);
                 startActivity(intent);
             }
